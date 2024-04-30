64c638b1b99e87e3df2b7ed071dc6d2ba1399c6c
Alexandru Rosca <alexandrurosca968@gmail.com>
2024-04-30 23:57:59 +0300

    Initial state of the backend, game works for any given word

---
+++ main.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+struct Node {
+    char letter;
+    bool show;
+    struct Node *next;
+};
+
+struct Node* initNode();
+void createListFromString(struct Node** head, char* string);
+void guessLetter(struct Node* head, int *livesLeft);
+bool isWinner(struct Node* head);
+bool modifiedShowStatus(struct Node* head, char c);
+void printList(struct Node* head);
+
+
+int main() {
+
+    struct Node* head = NULL;
+    int livesLeft = 5;
+
+    createListFromString(&head, "alphabet");
+
+    while ( livesLeft ) {
+        printList(head);
+        printf(" (%d lives left)", livesLeft);
+        guessLetter(head, &livesLeft);
+
+        if ( isWinner(head) ) {
+            printList(head);
+            printf("\n You win! ");
+            return 0;
+        }
+    }
+
+    printf("\n You lost! ");
+
+    return 0;
+}
+
+struct Node* initNode()
+{
+    struct Node* newNode = (struct Node *)malloc( sizeof(struct Node) );
+
+    newNode->show = false;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+void createListFromString(struct Node** head, char* string)
+{
+    if (strlen(string) == 0 ) // if the string is empty there is no list to create
+        return;
+
+    int i = 0;
+    struct Node* newNode = initNode();
+    newNode->letter = string[i++];
+
+    *head = newNode; // initiate the head of the list with the first letter of the string
+
+    // iterate through the rest of the string and assign a letter for each node of the list
+    while ( string[i] ) {
+        struct Node* tempNode = initNode();
+
+        tempNode->letter = string[i];
+
+        newNode->next = tempNode;
+        newNode = tempNode;
+
+        i++;
+    }
+
+}
+
+void guessLetter(struct Node* head, int *livesLeft)
+{
+    char c, newLine;
+
+    printf("\nGuess a letter: ");
+    fscanf(stdin, "%c%c", &c, &newLine);
+
+    if ( !modifiedShowStatus(head, c) ) // if no letter was guessed
+        *livesLeft -= 1;                // decrement the no of guesses left
+}
+
+bool isWinner(struct Node* head)
+{
+    struct Node* node = head;
+    bool condition = true; // we assume that every letter was guessed
+
+    while ( node != NULL ) {
+        if( node->show == false ) // if a not guessed letter is found
+            condition = false;    // condition is now false
+        node = node->next;
+    }
+
+    return condition;
+}
+
+bool modifiedShowStatus(struct Node* head, char c)
+{
+    struct Node* node = head;
+    bool isModified = false;
+
+    while ( node != NULL ) {
+        // if a letter from the string was guessed
+        if ( node->letter == c ) {
+            node->show = true; // change its show status
+            isModified = true; // and know not to decrement no of lives left
+        }
+        node = node->next;
+    }
+
+    return isModified;
+}
+
+void printList(struct Node* head)
+{
+    struct Node* node = head;
+
+    printf("\nYour word: ");
+
+    while ( node != NULL ) {
+        if( node->show )
+            printf("%c", node->letter);
+        else
+            printf("_");
+        node = node->next;
+    }
+}
